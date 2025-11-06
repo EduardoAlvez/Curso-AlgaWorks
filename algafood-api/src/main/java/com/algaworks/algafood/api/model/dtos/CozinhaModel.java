072e527a4dc02a312c8c3000495d5e77e5585b06
Eduardo Alvez <byteswood@gmail.com>
2025-11-05 22:15:10 -0300

    061 - Boas Praticas: Começando usar DTOs

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/dtos/CozinhaModel.java
@@ -0,0 +1,7 @@
+package com.algaworks.algafood.api.model.dtos;
+
+public class CozinhaModel {
+
+    private Long id;
+    private String nome;
+}
---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/dtos/RestauranteModel.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafood.api.model.dtos;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+public class RestauranteModel {
+
+    private Long id;
+    private String nome;
+    private CozinhaModel cozinha;
+
+}
